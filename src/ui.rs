a5f67102cf828fb4b309a4826ef51331fd9d3265
Bogay <pojay11523@gmail.com>
2025-06-14 19:03:19 +0800

    feat: render game grid

--- src/ui.rs
+++ src/ui.rs
@@ -1,13 +1,13 @@
+use crate::app::App;
+use crate::game::AllyElement;
 use ratatui::{
     buffer::Buffer,
-    layout::{Alignment, Rect},
-    style::{Color, Stylize},
+    layout::{Alignment, Constraint, Layout, Rect},
+    style::{Color, Style, Stylize},
     widgets::{Block, BorderType, Paragraph, Widget},
 };
 
-use crate::app::App;
-
-impl Widget for &App {
+impl Widget for &mut App {
     /// Renders the user interface widgets.
     ///
     // This is where you add new widgets.
@@ -16,24 +16,60 @@ impl Widget for &App {
     // - https://github.com/ratatui/ratatui/tree/master/examples
     fn render(self, area: Rect, buf: &mut Buffer) {
         let block = Block::bordered()
-            .title("nycu-gdc-game-jam-0th")
+            .title("Brainrot TD")
             .title_alignment(Alignment::Center)
             .border_type(BorderType::Rounded);
+        let inner_block = block.inner(area);
+        block.render(area, buf);
 
-        let text = format!(
-            "This is a tui template.\n\
-                Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-                Press left and right to increment and decrement the counter respectively.\n\
-                Counter: {}",
-            self.counter
-        );
+        const GRID_WIDTH: usize = 9;
+        const GRID_HEIGHT: usize = 5;
 
-        let paragraph = Paragraph::new(text)
-            .block(block)
-            .fg(Color::Cyan)
-            .bg(Color::Black)
-            .centered();
+        let row_constraints = vec![Constraint::Max(10); GRID_HEIGHT];
+        let grid_layouts = Layout::vertical(row_constraints)
+            .split(inner_block)
+            .iter()
+            .map(|&a| {
+                let col_constrains = vec![Constraint::Max(20); GRID_WIDTH];
+                Layout::horizontal(col_constrains).split(a).to_vec()
+            })
+            .collect::<Vec<_>>();
+        assert_eq!(GRID_HEIGHT, grid_layouts.len());
+        assert_eq!(GRID_WIDTH, grid_layouts[0].len());
 
-        paragraph.render(area, buf);
+        // render all cells first
+        for row in &grid_layouts {
+            for cell in row {
+                let p = Paragraph::new("")
+                    .block(Block::bordered())
+                    .style(Style::new().gray());
+                p.render(cell.clone(), buf);
+            }
+        }
+
+        // render ally grid
+        let game = self.game.as_ref().unwrap();
+        for row_i in 1..GRID_HEIGHT - 1 {
+            for col_i in 1..GRID_WIDTH - 1 {
+                let ally = &game.borad.ally_grid[row_i - 1][col_i - 1];
+                let text = match ally {
+                    Some(a) => a.level.to_string(),
+                    None => "".to_string(),
+                };
+                let style = match ally.as_ref().map(|a| &a.element) {
+                    Some(AllyElement::Basic) => Style::new().bg(Color::White),
+                    Some(AllyElement::Slow) => Style::new().bg(Color::LightBlue),
+                    Some(AllyElement::Dot) => Style::new().bg(Color::LightGreen),
+                    Some(AllyElement::Aoe) => Style::new().bg(Color::LightRed),
+                    Some(AllyElement::Critical) => Style::new().bg(Color::Yellow),
+                    None => Style::new().bg(Color::Gray),
+                };
+                let block = Block::bordered().style(style);
+                let p = Paragraph::new(text).block(block);
+
+                let rect = grid_layouts[row_i][col_i].clone();
+                p.render(rect, buf);
+            }
+        }
     }
 }
